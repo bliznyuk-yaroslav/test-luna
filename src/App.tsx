149228effb70be992c16f33731344b163eb7680f
Yaroslav Bliznyuk <144949333+bliznyuk-yaroslav@users.noreply.github.com>
2025-03-09 17:48:31 +0200

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import { useState } from "react";
+import StatusLabel, { Status } from "./component/StatusLabel/StatusLabel";
+import RootLayout from "./component/Layout/Layout";
+import "./index.css";
 
 function App() {
   return (
-    <div>
-      <p>Welcome to Luna Edge technical interview</p>
-    </div>
-  )
+    <RootLayout>
+      <div>
+        <p>Welcome to Luna Edge technical interview</p>
+        <StatusLabel status={Status.Active}>User is active</StatusLabel>
+      </div>
+    </RootLayout>
+  );
 }
 
-export default App
+export default App;
